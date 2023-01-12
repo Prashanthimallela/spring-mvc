2705101e9d0e9fc24709c3f788ffd4c92b0676db
Prashanthimallela <119045241+Prashanthimallela@users.noreply.github.com>
2023-01-12 23:23:31 +0530

    Add files via upload

---
+++ ProductMapper.java
@@ -0,0 +1,20 @@
+package com.training.springmvc.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class ProductMapper implements RowMapper<Product>{
+
+	public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		Product product = new Product();
+		product.setProdId(rs.getInt("prod_id"));
+		product.setProdName(rs.getString("prod_name"));
+		product.setProdDesc(rs.getString("prod_desc"));
+		product.setPrice(rs.getDouble("price"));
+		return product;
+	}
+
+}
